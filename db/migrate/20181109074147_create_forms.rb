86c4d440c5e05320e2a7bffbe55562905483d7ac
3ypup <posilay.pochashe@gmail.com>
2018-11-09 10:54:30 +0300

    added form

---
+++ db/migrate/20181109074147_create_forms.rb
@@ -0,0 +1,29 @@
+class CreateForms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :forms do |t|
+
+      t.text :name_child
+      t.datetime :date_of_birth
+      t.text :name_parent
+      t.text :phones_parent
+      t.text :phone_child
+      t.text :adress
+      t.text :email
+      t.text :food_allergy
+      t.text :vegan
+      t.text :psycho
+      t.text :events
+      t.text :also
+      t.text :bonus_contacts
+      t.text :friends
+      
+
+
+      t.string :image
+
+      
+
+      t.timestamps
+    end
+  end
+end
