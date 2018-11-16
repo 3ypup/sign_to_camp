f14ba6aaf7a0c48983b47c8d00d046bfbfbb64fb
3ypup <posilay.pochashe@gmail.com>
2018-11-16 10:38:01 +0300

    deleted validation phone_child

--- app/models/form.rb
+++ app/models/form.rb
@@ -7,7 +7,7 @@ acts_as_xlsx
   validates :date_of_birth, presence: {:message => "Введите дату рождения подростка!"}
   validates :name_parent, presence: {:message => "Введите имя родителей(опекунов)!"}
   validates :phones_parent, presence: {:message => "Введите контакты родителей(опекунов)!"}
-  validates :phone_child, presence: {:message => "Введите телефон подростка!"}
+  #validates :phone_child, presence: {:message => "Введите телефон подростка!"}
   validates :adress, presence: {:message => "Введите домашний адрес!"}
   validates :photo_child, presence: {:message => "Добавьте фотографию подростка!"}
   validates :photo_passport, presence: {:message => "Добавьте скан документа удостоверяющего личность!"}
