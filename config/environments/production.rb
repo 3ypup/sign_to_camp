9cda2847da1e9bda107a063d104c00b7f6b5f29c
3ypup <posilay.pochashe@gmail.com>
2021-05-20 15:07:56 +0300

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
 
-config.action_mailer.default_url_options = { :host => '185.189.12.126' }
+config.action_mailer.default_url_options = { :host => 'kosmalager.ru' }
 ActionMailer::Base.smtp_settings = {
 
 
